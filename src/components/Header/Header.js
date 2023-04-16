8365a7190f21e4e953b17d242eaee4a12e532026
JThorneX <115962097+JThorneX@users.noreply.github.com>
2023-04-15 22:59:12 -0600

    header css picture

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,12 @@ import React from "react";
 import CV from "../CVdownload";
 import sophie from "../../assets/sophie on fleek.png";
 import Socials from "../Socials";
+import "./header.css";
 
 function Header() {
   return (
     <header>
-      <div className="container header__container">
+      <div className="container header_container">
         <h5> Hello, I am</h5>
         <h1> Sophie Hall</h1>
         <h5 className="text-light">Medical Coder</h5>
