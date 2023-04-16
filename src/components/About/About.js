40a98d5c3cc5a0953accd1e888e08a76a9b6edfc
JThorneX <115962097+JThorneX@users.noreply.github.com>
2023-04-16 00:10:23 -0600

    about css

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,52 @@
 import React from "react";
+import "./about.css";
+import me from "../../assets/sophie grimace.png";
+import { FaQuestionCircle } from "react-icons/fa";
 
 function About() {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={me} alt="about me image" className="about_image" />
+          </div>
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaQuestionCircle className="about_icon" />
+              <h5>Category</h5>
+              <small>Content for category</small>
+            </article>
+            <article className="about_card">
+              <FaQuestionCircle className="about_icon" />
+              <h5>Category</h5>
+              <small>Content for category</small>
+            </article>
+            <article className="about_card">
+              <FaQuestionCircle className="about_icon" />
+              <h5>Category</h5>
+              <small>Content for category</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga iure
+            perspiciatis, deleniti inventore qui omnis voluptates vel molestiae
+            ipsa maxime molestias saepe quos, error laborum? Esse minus quam
+            mollitia eligendi?
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Contact Me
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
